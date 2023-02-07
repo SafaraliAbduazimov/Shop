4707dffeb18cbd7be45d94712845f3f4d59940df
SafaraliAbduazimov <02safmath05@gmail.com>
2023-02-07 16:02:39 +0300

    animation deleted

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -57,7 +57,7 @@
     </nav>
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <a href="#" class="brand-link">
-            <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <img src="{{asset('dist/img/photo2.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">Admin</span>
         </a>
         <div class="sidebar">
