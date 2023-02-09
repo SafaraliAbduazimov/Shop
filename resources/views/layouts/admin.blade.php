a0c28fd235e23d19f9d7d8b68bd2f31645684ff4
SafaraliAbduazimov <02safmath05@gmail.com>
2023-02-09 14:17:19 +0300

    icoka izmenil

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -23,13 +23,10 @@
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="{{route('home.index')}}" class="nav-link">Home</a>
+                <a href="{{route('contact.index')}}" class="nav-link"><i class="fa fa-phone-volume"> Contact</i></a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="{{route('contact.index')}}" class="nav-link">Contact</a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="{{route('about.index')}}" class="nav-link">About</a>
+                <a href="{{route('about.index')}}" class="nav-link"><i class="fa fa-snowflake"> About</i></a>
             </li>
         </ul>
         <ul class="navbar-nav ml-auto">
@@ -57,7 +54,7 @@
     </nav>
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <a href="#" class="brand-link">
-            <img src="{{asset('dist/img/photo2.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <img src="{{asset('dist/img/photo2.png')}}" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">Admin</span>
         </a>
         <div class="sidebar">
@@ -81,13 +78,29 @@
             </div>
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                    <li class="nav-header">ADMIN PANEL</li>
                     <li class="nav-item">
-                        <a href="{{route('clients.index')}}" class="nav-link">
-                            <i class="fas fa-align-justify"></i>
+                        <a href="{{route('home.index')}}" class="nav-link link-dark">
+                            <i class="fa fa-home"></i>
+                            <p>
+                                Home
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{route('clients.index')}}" class="nav-link link-dark">
+                            <i class="fa fa-user-circle"></i>
                             <p>
                                 Clients
-                                <span class="badge badge-info right">12</span>
+                                <span class="badge badge-info right">56</span>
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link link-dark">
+                            <i class="fa fa-th-list"></i>
+                            <p>
+                                Products
+                                <span class="badge badge-info right">110</span>
                             </p>
                         </a>
                     </li>
